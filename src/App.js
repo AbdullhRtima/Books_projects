db2f1d073b5385d49b8694d6ac67a495a75a3cfc
AbdullhRtima <abood.art.059@gmail.com>
2018-10-04 11:36:06 +0300

    editing landing page

--- src/App.js
+++ src/App.js
@@ -1,21 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
-import Navbar from './components/Navbar'
-import Silder from './components/Slider';
-import Books from './components/Books';
-import Footer from './components/Footer';
 import { Router , Route} from 'react-router';
+import Landingpage from './components/Landingpage'
 
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      <Navbar />
-      <Silder/>
-      <br/>
-      <Books/>
-      <Footer/>
+     <Landingpage/>
       </div>
       
       
---
+++ src/components/Landingpage.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+import Navbar from './Navbar'
+import Slider from './Slider'
+import Books from './Books'
+import Footer from './Footer'
+
+export class Landingpage extends Component {
+  render() {
+    return (
+      <div>
+        <Navbar/>
+        <Slider/>
+        <br/>
+        <Books/>
+        <Footer/>
+      </div>
+    )
+  }
+}
+
+export default Landingpage
