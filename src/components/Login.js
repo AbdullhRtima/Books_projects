0745d3284cd9871caefdf5b671ebd1a1f84b88a6
mahmoud <m0597731363@gmail.com>
2018-11-18 17:52:13 +0200

    trying to use fetch api insted of axios

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,98 +1,138 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 import axios from 'axios';
 
-import { Card ,InputGroup, InputGroupAddon, InputGroupText, Input , Container,
-   Row, Col,Button, Form, FormGroup, Label, FormText } from 'reactstrap';
+import {
+  Card,
+  InputGroup,
+  InputGroupAddon,
+  InputGroupText,
+  Input,
+  Container,
+  Row,
+  Col,
+  Button,
+  Form,
+  FormGroup,
+  Label,
+  FormText
+} from 'reactstrap';
 
-import { setInStorage } from '../helpers/storage.js';
+import {setInStorage} from '../helpers/storage.js';
 
 export class Login extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
-      email:"",
-      password:"",
-      loginError:""
+      email: "",
+      password: "",
+      loginError: ""
     }
   }
   onChange = event => {
     switch (event.target.name) {
       case "email":
-        this.setState({
-          email: event.target.value
-        })
+        this.setState({email: event.target.value})
         break;
       case "password":
-        this.setState({
-          password: event.target.value,
-          loginError:""
-        })
+        this.setState({password: event.target.value, loginError: ""})
         break;
       default:
-        this.setState({
-          loginError:""
-        })
+        this.setState({loginError: ""})
     }
   }
 
   onSubmit = event => {
     event.preventDefault();
-    const headers = {
-      'Access-Control-Allow-Origin': '*',
-      "changeOrigin": true
-    }
-    axios.post('https://stormy-eyrie-81072.herokuapp.com/api/auth/login',
+    fetch('https://stormy-eyrie-81072.herokuapp.com/api/auth/login', {
+      method: 'POST',
+      headers: {
+        'Access-Control-Allow-Credentials':true,
+        'Access-Control-Allow-Origin':"*",
+        "Content-Type": "application/json",
+        'Accept': "application/json"
+      },
+      'credentials': 'include',
+      'mode': 'cors',
+      body: JSON.stringify(
         {
-             email: this.state.email,
-             password: this.state.password
+          email: this.state.email,
+          password: this.state.password
         }
-    )
-     .then(res => {
-       console.log('aaaaaaa');
-       if(res.status === 200){
-          setInStorage("token", res.data.token);
-          this.props.history.push('/profile');
-       }
-     })
-     .catch(err => {
-       console.log(err);
-       this.setState({
-           loginError : "User name or password is invalid! ",
-         })
-     })
+      )
+    })
+    .then(response =>
+      {
+        console.log(response, 'response is here');
+        response.json()
+      })
+    .then(response => console.log('Success:', JSON.stringify(response)))
+    .catch(error =>
+        {
+       console.error('Error:', error)});
+
+    // const headers = {
+    //   'Access-Control-Allow-Origin': '*',
+    //   "changeOrigin": true
+    // }
+    // axios.post('https://stormy-eyrie-81072.herokuapp.com/api/auth/login',
+    //     {
+    //          email: this.state.email,
+    //          password: this.state.password
+    //     }
+    // )
+    //  .then(res => {
+    //    console.log('aaaaaaa');
+    //    if(res.status === 200){
+    //       setInStorage("token", res.data.token);
+    //       this.props.history.push('/profile');
+    //    }
+    //  })
+    //  .catch(err => {
+    //    console.log(err);
+    //    this.setState({
+    //        loginError : "User name or password is invalid! ",
+    //      })
+    //  })
   }
 
   render() {
     const {loginError} = this.state
-    return (
-      <Container style={{ padding: '.5rem' , marginTop : 40 , textAlign :'right'  }}>
+    return (<Container style={{
+        padding: '.5rem',
+        marginTop: 40,
+        textAlign: 'right'
+      }}>
       <Row>
-      <Col  xs='3'></Col>
+        <Col xs='3'></Col>
         <Col>
-      <Form onSubmit={this.onSubmit} className="login">
-        <h1>  تسجيل دخول </h1>
-        <FormGroup>
-          <Label for="exampleEmail">البريد الالكتروني </Label>
-          <Input onChange={this.onChange} type="email" name="email" placeholder="example@host.com" />
-        </FormGroup>
-        <FormGroup>
-          <Label for="examplePassword">كلمة المرور</Label>
-          <Input onChange={this.onChange} type="password" name="password"  placeholder="كلمة المرور" />
-        </FormGroup>
-        <a href='/recovery'>
-          <h5>هل نسيت كلمة المرور ؟</h5>
-        </a>
-        {
-          loginError ?
-            <div>{loginError}</div>:(null)
-        }
-        <Button type='submit' >تسجيل دخول </Button>
-      </Form>
-      </Col>
-      <Col  xs='3'></Col>
+          <Form onSubmit={this.onSubmit} className="login">
+            <h1>
+              تسجيل دخول
+            </h1>
+            <FormGroup>
+              <Label for="exampleEmail">البريد الالكتروني
+              </Label>
+              <Input onChange={this.onChange} type="email" name="email" placeholder="example@host.com"/>
+            </FormGroup>
+            <FormGroup>
+              <Label for="examplePassword">كلمة المرور</Label>
+              <Input onChange={this.onChange} type="password" name="password" placeholder="كلمة المرور"/>
+            </FormGroup>
+            <a href='/recovery'>
+              <h5>هل نسيت كلمة المرور ؟</h5>
+            </a>
+            {
+              loginError
+                ? <div>{loginError}</div>
+                : (null)
+            }
+            <Button type='submit'>تسجيل دخول
+            </Button>
+          </Form>
+        </Col>
+        <Col xs='3'></Col>
       </Row>
-      </Container>
-    )
+    </Container>)
   }
 }
 
